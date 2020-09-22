088fde50d7e87d72d2b30858afe5bcef96a392eb
David Jimenez <davidjimenez@Davids-MacBook-Pro.local>
2020-09-21 22:15:41 -0500

    Version 1.0.0

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const config = require('config');
 const path = require('path');
 const cors = require('cors');
 require('dotenv').config();
